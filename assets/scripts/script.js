b408daef94a4bbdc64c454f28d1c44b1d9cc58db
Edward <herreraed93@gmail.com>
2022-12-03 12:30:10 -0800

    finish functionality for 5day forecast

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 var city = document.querySelector(".city_search")
 var apiKey = '83ba888341c4c3c5f2ce6316ad28fbfd'
 var searchForm = document.querySelector(".search")
-
+var fiveDay = document.getElementById("five_day")
 
 
 
@@ -63,7 +63,30 @@ function displayForecast(data){
     for(var i=0; i< data.list.length; i +=8 ){
         const {name} = data.city;
         console.log(name)
+        var nameEl=document.createElement('p')
+        nameEl.append(name)
+        fiveDay.appendChild(nameEl)
+        const {temp,humidity} =data.list[i].main;
+        console.log(temp)
+        var tempEl=document.createElement('p')
+        var humidEl=document.createElement('p')
+        humidEl.append(humidity)
+        tempEl.append(temp)
+        fiveDay.appendChild(tempEl)
+        fiveDay.appendChild(humidEl)
+        const{description} =data.list[i].weather[0];
+        var descEl=document.createElement('p')
+        descEl.append(description)
+        fiveDay.appendChild(descEl)
+        console.log(description)
+        console.log(humidity)
         const {speed} = data.list[i].wind;
+        var speedEl=document.createElement('p')
+        speedEl.append(speed)
+        fiveDay.appendChild(speedEl)
         console.log(speed)
     } 
+
+    
 }
+
